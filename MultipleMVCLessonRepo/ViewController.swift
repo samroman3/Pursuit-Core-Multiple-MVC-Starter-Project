ff5888f300f9516e2b6122a7c011fca36e03f25d
Sam <samroman3@gmail.com>
2019-08-13 11:09:05 -0400

    added MovieViewController and TableView Controller

--- MultipleMVCLessonRepo/ViewController.swift
+++ MultipleMVCLessonRepo/ViewController.swift
@@ -8,7 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
